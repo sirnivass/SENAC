f61a724e0c4a3bdfc5e5cb16a7bb5c5534bd77a3
sirnivass <sirnivass@gmail.com>
2022-11-13 00:47:38 -0300

    Fatorial

---
+++ UC - 05/Atividade/Aprendendo/Desafio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace FactorialExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("N: ");
+            int N = int.Parse(Console.ReadLine());
+            int fact = 1;
+            while (true)
+            {
+                Console.Write(N);
+                if (N == 1)
+                {
+                    break;
+                }
+                Console.Write("*");
+                fact *= N;
+                N--;
+            }
+            Console.WriteLine(" = {0}", fact);
+        }
+    }
+}
