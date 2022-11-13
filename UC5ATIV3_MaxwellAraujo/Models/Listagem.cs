0d17e294d8ea2d1dec8cee49dcb14fd2e5fe3ac5
sirnivass <sirnivass@gmail.com>
2022-11-12 21:07:58 -0300

    Programa em C# que verifica maioridade do usuário

---
+++ UC5ATIV3_MaxwellAraujo/Models/Listagem.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UC5ATIV3_MaxwellAraujo.Models
+{
+    public class Listagem
+    {
+        public string descricao {get; set;}
+        public double valorUnitario {get; set;}
+        public int quantidade {get; set;}
+
+    }
+}
